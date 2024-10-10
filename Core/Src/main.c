d6c31d148994d54ee90eba24868691d3a4441730
Afanasenko Alexander <aomiki@petalmail.com>
2024-10-10 20:44:34 +0400

    UART messages

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,6 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,6 +49,7 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -101,6 +103,12 @@ void signal_on_interval(GPIO_TypeDef* pin_type, uint16_t pin, GPIO_PinState pin_
   	  HAL_Delay(interval);
     }
 }
+
+void uart_send_message(unsigned char * msg)
+{
+    HAL_UART_Transmit(&huart1, msg, strlen(msg), HAL_MAX_DELAY);
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -132,6 +140,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -140,14 +149,20 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  uart_send_message("---------Routine started---------\n\r");
+
 	  signal_on_interval(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET, 50, 10);
 	  signal_on_interval(GPIOA, GPIO_PIN_0, GPIO_PIN_SET, 50, 10);
 
 	  signal_sos(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET, 250);
+	  uart_send_message("SOS pin 13\n\r");
 	  HAL_Delay(1000);
 
 	  signal_sos(GPIOA, GPIO_PIN_0, GPIO_PIN_SET, 200);
+	  uart_send_message("SOS pin 0\n\r");
 	  HAL_Delay(1000);
+
+	  uart_send_message("---------Routine finished---------\n\r");
 
     /* USER CODE END WHILE */
 
@@ -190,6 +205,39 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
